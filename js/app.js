109f249626bcd044fb12aedd9fc2c2c3225cca68
Leandro Parazito <leandroh@gmail.com>
2014-09-04 14:22:11 -0300

    first tests

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("sampleApp", ["firebase"]);
+
+app.controller("SampleCtrl", ["$scope", "$firebase", function($scope, $firebase) {
+  var ref = new Firebase("https://resplendent-fire-4324.firebaseio.com/messages");
+  var sync = $firebase(ref);
+
+  $scope.messages = sync.$asArray();
+
+  $scope.addMessage = function(text) {
+    $scope.messages.$add({text: text});
+  }
+}]);
