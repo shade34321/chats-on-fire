459ee2350da55f0dcd81a4315b0c937f296d2e7c
Leandro Parazito <leandroh@gmail.com>
2014-09-17 16:56:59 -0300

    overflow declaration

--- js/app.js
+++ js/app.js
@@ -4,6 +4,27 @@ app.controller("SampleCtrl", ["$scope", "$firebase", function($scope, $firebase)
   var ref = new Firebase("https://resplendent-fire-4324.firebaseio.com/messages");
   var sync = $firebase(ref);
 
+  var setTitle = function() {
+    var title = document.title;
+    document.title = (title == "nova" ? "mensagem" : "nova");
+  }
+
+  var handler = ref.on("child_added", function(snap) {
+    setInterval(setTitle, 1000);
+  });
+
+  var activeMethod = (function() {
+    clearInterval(setTitle);
+    document.title = "Chat's On Fire!";
+  });
+
+  window.onclick = activeMethod;
+  window.onmousemove = activeMethod;
+  window.onmouseenter = activeMethod;
+  window.onkeydown = activeMethod;
+  window.onscroll = activeMethod;
+  window.onmousewheel = activeMethod;
+
   $scope.messages = sync.$asArray();
 
   $scope.addMessage = function(text) {
